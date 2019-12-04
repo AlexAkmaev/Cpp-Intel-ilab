35c9367c47f55ce697a10fa6195e42a5bf074a1f
AlexAkmaev <permafried2000@gmail.com>
2019-12-04 20:13:37 +0300

    3 level, Vulkan usage

--- Triangle Intersect/P2_N_triangs.cpp
+++ Triangle Intersect/P2_N_triangs.cpp
@@ -4,46 +4,6 @@
 #include "Geometry.hpp"
 using namespace std;
 
-template <typename T>
-set<int> intersecting_triangles(const vector<Triangle<T>>& trgs) {
-	set<int> res;
-	vector<pair<Point<T>, int>> spheres;
-	T max_R = 0;
-
-	for (int i = 0; i < trgs.size(); ++i) {
-		const Sphere<T> s{trgs[i]};
-		spheres.push_back(pair<Point<T>, int>{s.centr, i});
-		T r;
-		if ((r = s.R) > max_R)
-		    max_R = r;
-	}
-	T max_D = 2*max_R, dist_x = max_D;
-
-	sort(spheres.begin(), spheres.end(), comp_x<T>());
-	auto it_x = spheres.begin(), x_beg = it_x, x_end = it_x;
-	auto it_end = spheres.end();
-	int k = 0;
-	auto trgs_beg = trgs.begin();
-
-	while (it_x != it_end) {
-		sort(x_beg, x_end, comp_x<T>());
-		if (k == 0) {
-			x_end = lower_bound(x_beg, it_end, pair<Point<T>, int>{Point<T>{dist_x + max_D, 100, 100}, 0}, comp_x<T>());
-			++k;
-		} else {
-			x_beg = lower_bound(x_beg, it_end, pair<Point<T>, int>{Point<T>{dist_x - max_D, 100, 100}, 0}, comp_x<T>());
-			x_end = lower_bound(x_beg, it_end, pair<Point<T>, int>{Point<T>{dist_x + max_D, 100, 100}, 0}, comp_x<T>());
-		}
-
-		area_y(x_beg, x_end, max_D, trgs_beg, res);
-
-		it_x = x_end;
-		dist_x += max_D;
-	}
-
-	return res;
-}
-
 int main() {
 	vector<Triangle<double>> trgs;
 	int N;
